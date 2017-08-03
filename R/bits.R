145a62fa7c10c11a175732d456896e8900b6be94
Romain Francois <romain@thinkr.fr>
2017-08-03 07:35:45 +0200

    + documentation about `bytes` and `runes`

--- R/bits.R
+++ R/bits.R
@@ -1,18 +1,38 @@
 
+prepend_class <- function(., class){
+  class(.) <- c(class,class(.))
+  .
+}
+
 raw_to_bits <- function(.){
   paste(substr(as.character(rev(rawToBits(.))), 2, 2), collapse = "")
 }
 
+#' Split a utf-8 encoded string into bytes
+#'
+#' @param s a utf-8 encoded string
+#'
+#' @return a tibble (with extra class "tbl_bytes") with columns
+#' - id : index of the byte
+#' - byte: hex representation of the byte
+#' - decimal: decimal reprsentation of the byte
+#' - binary: binary representation of the byte
+#'
+#' @examples
+#' bytes( "hello" )
+#'
 #' @importFrom tibble tibble
 #' @importFrom purrr map_chr
 #' @export
-bits <- function(s){
+bytes <- function(s){
   bytes <- charToRaw(s)
-  structure( tibble(
-    decimal = as.integer(bytes),
-    hex     = bytes,
-    binary  = map_chr(bytes, raw_to_bits)
-  ), class = c("tbl_bits", "tbl_df", "tbl", "data.frame") )
+  tibble(
+      id      = seq_along(bytes),
+      byte    = bytes,
+      decimal = as.integer(bytes),
+      binary  = map_chr(bytes, raw_to_bits)
+    ) %>%
+    prepend_class("tbl_bytes" )
 }
 
 #' @importFrom stringr str_replace
@@ -22,9 +42,22 @@ extract_rune <- function( bits ){
     paste( collapse = "")
 }
 
+#' Split a string into unicode runes
+#'
+#' @param s a utf-8 encoded string
+#'
+#' @return a tibble (with extra class "tbl_runes") with columns:
+#' - id: index of the rune in the input
+#' - rune: name of the rune, i.e. an hex representation prefixed by "U+"
+#' - rune_binary: binary representation of the rune
+#' - rune_decimal: decimal representation of the rune, i.e. the index of the character in the unicode table
+#' - utf8_bytes: hex representation of the utf8 bytes
+#' - utf8_binary: binary representation of the utf8 bytes
+#' - description: description of the rune, from [uni::code]
+#'
 #' @importFrom magrittr %>%
 #' @importFrom purrr map map_int
-#' @importFrom dplyr group_by summarise mutate select
+#' @importFrom dplyr group_by summarise mutate select left_join
 #' @importFrom tidyr nest
 #' @export
 runes <- function(s){
@@ -49,9 +82,10 @@ runes <- function(s){
       rune_binary  = map_chr( bits, extract_rune ),
       rune_decimal = strtoi(rune_binary, base = 2),
       rune         = sprintf( "U+%04X", rune_decimal ),
-      hex          = map_chr( bytes, ~ paste( sprintf("%02X", as.integer(.)), collapse = " " ) )
+      utf8_bytes   = map_chr( bytes, ~ paste( sprintf("%02X", as.integer(.)), collapse = " " ) ),
+      utf8_binary  = map_chr( bits, ~ paste( map_chr(., ~paste(as.numeric(.), collapse = "") ), collapse = " " ) )
     ) %>%
-    select( id, rune, hex, rune_binary, rune_decimal ) %>%
+    select( id, rune, rune_binary, rune_decimal, utf8_bytes, utf8_binary ) %>%
+    left_join( select(uni::code, rune, description) , by = "rune" ) %>%
     structure( class = c( "tbl_runes", class(.) ) )
-
 }
